662daa324c2f66282805d585e61686fd7093eaef
moaad_isn't_my_name <78547347+TheUniqueMohssine@users.noreply.github.com>
2026-06-26 18:42:46 +0100

    Update gemi.py to clone repository instead of open

--- gemi.py
+++ gemi.py
@@ -1,5 +1,5 @@
 import webbrowser
 import os
-os.system('https://github.com/TheUniqueMohssine/gemi_js_py.git')
+os.system('git clone https://github.com/TheUniqueMohssine/gemi_js_py.git')
 os.chdir('gemi_js_py')
 webbrowser.open('index.html')
