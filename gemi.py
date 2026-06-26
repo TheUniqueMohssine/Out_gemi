d263af7b42d0c8cf3078e72dc92a880c4aeb5958
moaad_isn't_my_name <78547347+TheUniqueMohssine@users.noreply.github.com>
2026-06-26 18:40:42 +0100

    Update gemi.py

--- gemi.py
+++ gemi.py
@@ -1,5 +1,5 @@
 import webbrowser
 import os
-os.system('git clone https://github.com/moaad77/gemi_js_py.git')
+os.system('https://github.com/TheUniqueMohssine/gemi_js_py.git')
 os.chdir('gemi_js_py')
 webbrowser.open('index.html')
