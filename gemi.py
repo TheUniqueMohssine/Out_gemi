53dac60e68b30a4e43d17770b8e72dcd40851efa
moaad_isn't_my_name <78547347+TheUniqueMohssine@users.noreply.github.com>
2026-06-26 19:33:44 +0100

    Fix formatting issues in gemi.py

--- gemi.py
+++ gemi.py
@@ -4,7 +4,7 @@ import os
 os.system('git clone https://github.com/TheUniqueMohssine/gemi_js_py.git')
 os.chdir('gemi_js_py')
 webbrowser.open('index.html')
- os.remove('index.html')
+ 
 
 os.chdir('..')
 shutil.rmtree('gemi_js_py') 
