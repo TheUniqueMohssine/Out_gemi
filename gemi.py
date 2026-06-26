6bdd65fff3ba72eb44ebd410c2caaebad35638df
moaad_isn't_my_name <78547347+TheUniqueMohssine@users.noreply.github.com>
2026-06-26 19:16:22 +0100

    Implement cleanup of cloned gemi_js_py directory
    Added cleanup step to remove cloned repository after use.

--- gemi.py
+++ gemi.py
@@ -1,6 +1,10 @@
 import webbrowser
+import shutil
 import os
 os.system('git clone https://github.com/TheUniqueMohssine/gemi_js_py.git')
 os.chdir('gemi_js_py')
 webbrowser.open('index.html')
  os.remove('index.html')
+
+os.chdir('..')
+shutil.rmtree('gemi_js_py') 
